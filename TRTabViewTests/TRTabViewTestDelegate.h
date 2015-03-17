0123eab07e08d39c70897c994c2490bfc182adef
Francis Chong <francis@ignition.hk>
2015-03-17 15:39:52 +0800

    Handling case where user delete a tab before current selected index: in such case selected index should reduce by one.

---
+++ TRTabViewTests/TRTabViewTestDelegate.h
@@ -0,0 +1,13 @@
+//
+//  TRTabViewTestDelegate.h
+//  TRTabViewExample
+//
+//  Created by Francis Chong on 17/3/15.
+//  Copyright (c) 2015 Matthias Keiser. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TRTabViewTestDelegate : NSObject
+
+@end
---
+++ TRTabViewTests/TRTabViewTestDelegate.m
@@ -0,0 +1,13 @@
+//
+//  TRTabViewTestDelegate.m
+//  TRTabViewExample
+//
+//  Created by Francis Chong on 17/3/15.
+//  Copyright (c) 2015 Matthias Keiser. All rights reserved.
+//
+
+#import "TRTabViewTestDelegate.h"
+
+@implementation TRTabViewTestDelegate
+
+@end
